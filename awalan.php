7868a662c27fdf40f43037dd8b09a46a8f898b5a
dimzzj7711 <dimzzj357347@gmail.com>
2025-09-10 08:44:23 +0700

    katalog.produk

---
+++ awalan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="">
+<body>
+   
+
+
+
+
+
+</body>
+</html
---
+++ page/home.php
@@ -0,0 +1,126 @@
+<div class="text-center">
+  <div class="hero-container">
+  <img class="img-h"src="WhatsApp Image 2025-08-06 at 07.44.31.jpeg" alt="">
+  <h1 class="hero-text">Ortuseight</h1>
+</div>
+</div>
+
+
+
+ <br><br>
+<div class="text-center">
+  <h1 style="color: #e77529; font-family: 'Poppins', sans-serif; font-weight: 800;">DISKON</h1>
+</div>
+
+ <br>
+ <div class="padding">
+  <div class="row row-cols-1 row-cols-md-4 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 5.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">RP.850.000</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="11010667_CATALYST_LEGION_V5_FG_-_OFFWHITE_GOLDArtboard_1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title " style="font-family: 'Irish Grover';">CATALYST LEGION V5 FG
+          Off White Gold</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 3.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 4.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+ <br>
+
+<!--Bagian Gambar-->
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="upscalemedia-transformed (1).jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="upscalemedia-transformed (2).jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="upscalemedia-transformed.jpeg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<br>
+<div class="text-center">
+  <h1 style= "color: #e77529; font-family: 'Poppins', sans-serif; font-weight: 800;">BRAIN NEW</h1>
+</div>
+
+<br>
+<!--Bagian Diskon-->
+<div class="row row-cols-1 row-cols-md-4 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 5.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">RP.850.000</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="11010667_CATALYST_LEGION_V5_FG_-_OFFWHITE_GOLDArtboard_1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title " style="font-family: 'Irish Grover';">CATALYST LEGION V5 FG
+          Off White Gold</h5>
+        <p class="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 3.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="GAMBAR 4.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title" style="font-family: 'Irish Grover';">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<br><br>

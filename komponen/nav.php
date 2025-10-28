4081dc9e2bd893d25c71e702dc0d9a248c7b5641
dimzzj7711 <dimzzj357347@gmail.com>
2025-10-28 15:06:38 +0700

    dashboard-admin

--- komponen/nav.php
+++ komponen/nav.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
 
-        <img src="Vector_Logo_Collection_Eight_Sports_Brands_by_-removebg-preview.png" alt="Bootstrap" width="75" height="75">
+        <img src="img/Vector_Logo_Collection_Eight_Sports_Brands_by_-removebg-preview.png" alt="Bootstrap" width="75" height="75">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>

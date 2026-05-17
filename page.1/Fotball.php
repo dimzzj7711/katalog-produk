3202771a9c42c2d6b3e53a82aed5659faa256bba
dimzzj7711 <dimzzj357247@gmail.com>
2026-05-17 12:17:22 +0700

    update

---
+++ page.1/Fotball.php
@@ -0,0 +1,235 @@
+<div class="images-shop">
+<img src="img/Shop.png" alt="">
+</div>
+
+<section class="filter-bar">
+
+  <div class="filter-item">
+    <a href="#">PRICE</a>
+  </div>
+
+  <div class="filter-item">
+    <a href="#">COLOR</a>
+  </div>
+
+  <div class="filter-item">
+    <a href="#">SIZE</a>
+  </div>
+
+  <div class="filter-item">
+    <a href="#">RECOMMENDED</a>
+  </div>
+
+  <div class="filter-item">
+    <a href="#">CLEAR FILTER</a>
+  </div>
+
+  <div class="filter-item">
+    <a href="#">SORT BY</a>
+  </div>
+
+</section>
+
+<section class="product-section">
+
+  <div class="product-container">
+
+    <!-- PRODUCT 1 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST IMPERIUM FG</h3>
+        <p>ARGENT GREY LAVENDER</p>
+        <span>IDR 449,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 2 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <!-- <div class="product-thumbnail">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+      </div> -->
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>SALMON NAVY WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 3 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>AQUA PINK WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 4 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>FORTE NEXUS FG</h3>
+        <p>SILVER</p>
+        <span>IDR 549,000</span>
+      </div>
+    </div>
+
+  </div>
+
+</section>
+
+<section class="product-section">
+
+  <div class="product-container">
+
+    <!-- PRODUCT 1 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST IMPERIUM FG</h3>
+        <p>ARGENT GREY LAVENDER</p>
+        <span>IDR 449,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 2 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <!-- <div class="product-thumbnail">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+      </div> -->
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>SALMON NAVY WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 3 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>AQUA PINK WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 4 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>FORTE NEXUS FG</h3>
+        <p>SILVER</p>
+        <span>IDR 549,000</span>
+      </div>
+    </div>
+
+  </div>
+
+</section>
+
+<section class="product-section">
+
+  <div class="product-container">
+
+    <!-- PRODUCT 1 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST IMPERIUM FG</h3>
+        <p>ARGENT GREY LAVENDER</p>
+        <span>IDR 449,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 2 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <!-- <div class="product-thumbnail">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+        <img src="img/s2.png" alt="">
+      </div> -->
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>SALMON NAVY WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 3 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>CATALYST LEGION V5 FG</h3>
+        <p>AQUA PINK WHITE</p>
+        <span>IDR 379,000</span>
+      </div>
+    </div>
+
+    <!-- PRODUCT 4 -->
+    <div class="product-card">
+      <div class="product-image">
+        <img src="img/Fotball.jpg" alt="">
+      </div>
+
+      <div class="product-info">
+        <h3>FORTE NEXUS FG</h3>
+        <p>SILVER</p>
+        <span>IDR 549,000</span>
+      </div>
+    </div>
+
+  </div>
+
+</section>
